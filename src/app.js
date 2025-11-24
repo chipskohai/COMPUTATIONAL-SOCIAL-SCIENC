4834f1626a665e5ea1c170ac7dc3214930c60ae6
thommy <ngothommy@gmail.com>
2025-11-24 02:59:33 +0100

    testing git

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.get("/test", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "views/index.html"));
 });
 
 app.listen(
